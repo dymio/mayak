de0f0a1a6a642ab75f6210fbe8950a564f218d04
Sergey Haritonov <enhray@gmail.com>
2014-04-22 15:48:13 +0800

    Fixed slug and lead generation in NewsItem model

--- app/models/news_item.rb
+++ app/models/news_item.rb
@@ -3,36 +3,31 @@ class NewsItem < ActiveRecord::Base
   validates_presence_of :content, :title
   validates_uniqueness_of :slug
   before_save :generate_slug
+  before_save :generate_lead
 
   def find_by_slug slug
   	NewsItem.find(:first, :conditions => ['slug = ?', slug])
   end
 
-  def get_lead
-  	if not lead.nil? and not lead.empty?
-  		return lead
-  	else
-  		# there's no lead, so we try to find first paragraph of content 
-  		# http://stackoverflow.com/a/9661504
-  		return content[/#{Regexp.escape('<p>')}(.*?)#{Regexp.escape('</p>')}/m, 1]
-  	end
-  end
-
 private
 
-  def generate_slug
-  	slug ||= make_auto_slug
+  def generate_lead
+  	if self.lead.blank?
+	  	# there's no lead, so we try to find first paragraph of content 
+	  	# http://stackoverflow.com/a/9661504
+	  	self.lead = content[/#{Regexp.escape('<p>')}(.*?)#{Regexp.escape('</p>')}/m, 1]
+	  end
   end
 
-  def make_auto_slug
-    if self.slug.blank?
-      self.slug = Russian::transliterate(self.title).
-        downcase.
-        gsub(/[^a-z0-9]/, '-'). # change all incorrect symbols to '-'
-        gsub(/[-]{2,}/,'-').    # remove all several '-' in line
-        gsub(/^[-]+/,'').       # remove all '-' from start of string
-        gsub(/[-]+$/, '')       # remove all '-' from end of string
-    end
+  def generate_slug
+  	if self.slug.blank?
+  		self.slug = Russian::transliterate(self.title).
+	      downcase.
+	      gsub(/[^a-z0-9]/, '-'). # change all incorrect symbols to '-'
+	      gsub(/[-]{2,}/,'-').    # remove all several '-' in line
+	      gsub(/^[-]+/,'').       # remove all '-' from start of string
+	      gsub(/[-]+$/, '')       # remove all '-' from end of string
+  	end
   end
 
 end
