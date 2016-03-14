63edb360a4ba59df81dd52f227fe544a2f8886de
Ivan Dymkov <mstrdymio@gmail.com>
2016-03-14 19:55:26 +0700

    Add usefull comments to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,26 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# def seedfile(fname)
-#   File.open File.join(Rails.root, "public/system/seeds/", fname)
+# def time_point_string
+#   Time.now.strftime("%H:%M:%S:%L")
 # end
+
+# def seedfile(fname)
+#   File.open File.join(Rails.root, "public/content/seeds/", fname)
+# end
+
+# puts "#{time_point_string}: Start seeding"
+
+# # Use code of file 'db/seeds_more.rb'. This is way to split big seeds.rb file.
+# # You can use instance variables (@something) declared in that file.
+# require_relative 'seeds_more'
+
+# = - = - = - = - = - = - = - = - = - = - = - = - = - = - = - = - = - = - = --
+# puts "#{time_point_string}: seed Static Files"
+# = - = - = - = - = - = - = - = - = - = - = - = - = - = - = - = - = - = - = --
 
 # sig = StaticFile.new
 # sig.file = seedfile "example_pic.jpg"
 # sig.save
+
+# puts "#{time_point_string}: Seeding is done!"
