146fe77dd4b3fb9f5b92158bfa30b6ea5a7d8491
Ivan Dymkov <mstrdymio@gmail.com>
2017-03-10 20:47:40 +0700

    Add ability to set many email addresses to manager_email setting

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -4,6 +4,19 @@ class ApplicationMailer < ActionMailer::Base
 
   before_action :get_site_name
 
+  protected
+
+  # Method splits string of emails by comma or semicolon to array
+  #  if it only one email in string method will return string with the email
+  def recipients_from_string(emails_str)
+    rec_strs = emails_str.to_s.split(/[\;\,]+/)
+    rec_strs.map! do |rs|
+      ValidateEmail.valid?(rs.strip) ? rs.strip : nil
+    end
+    rec_strs.compact!
+    rec_strs.length == 1 ? rec_strs[0] : rec_strs
+  end
+
   private
 
   def get_site_name
--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -1,11 +1,14 @@
 class FeedbackMailer < ApplicationMailer
   def feedback_message(feedback)
-    manager_email = SettingProvider.instance.value_of :manager_email
-    unless ValidateEmail.valid?(manager_email)
+    recipients = recipients_from_string SettingProvider.instance.
+                                                        value_of(:manager_email)
+    if recipients.blank?
       raise StandardError, "There is no correct manager_email."
     end
+
     @feedback = feedback
-    mail(to: manager_email, subject: "Feedback from #{@site_name}") do |format|
+
+    mail(to: recipients, subject: "Feedback from #{@site_name}") do |format|
       format.html
       format.text
     end
